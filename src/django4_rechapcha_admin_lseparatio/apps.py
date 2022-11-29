dde73738418b000abbd3e144dd76c941631a68f7
Ionut Zapototchi <67255838+lseparatio@users.noreply.github.com>
2022-11-29 17:26:44 +0000

    first commit

---
+++ src/django4_rechapcha_admin_lseparatio/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Django4RechapchaAdminLseparatioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django4_rechapcha_admin_lseparatio'
